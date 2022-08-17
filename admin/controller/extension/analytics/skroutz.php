bfa085dfe3325f0d3adcc42b714d5381c366d17e
Prionysis <pasenidis.dionisios@gmail.com>
2022-08-17 04:12:13 +0300

    Add install.xml and update version

--- admin/controller/extension/analytics/skroutz.php
+++ admin/controller/extension/analytics/skroutz.php
@@ -4,7 +4,7 @@
  * Shopflix Analytics
  * @author Prionysis
  * @website https://github.com/Prionysis
- * @version 1.0
+ * @version 1.1
  */
 
 class ControllerExtensionAnalyticsSkroutz extends Controller
